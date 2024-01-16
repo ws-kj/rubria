e547695be6ea71207af7690786334747420db0b0
Will <wsavage6316@gmail.com>
2024-01-16 12:57:05 -0500

    registration backend

--- rubria-app/api/api.py
+++ rubria-app/api/api.py
@@ -1,20 +1,20 @@
 from typing import Union, Dict, Any
 from flask import Flask, request, g, session 
 from flask_bcrypt import Bcrypt
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import sqlite3
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 CORS(app, supports_credentials=True)
 app.config["CORS_HEADERS"] = "Content-Type"
+app.secret_key = "testing"
 dbpath = "rubria.db"
 
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
-        db = g._database = sqlite3.connect(dbpath)
-    db.row_factory = sqlite3.Row
+        db = g._database = sqlite3.connect(dbpath, isolation_level=None)
     return db
 
 def query_db(query, args=()):
@@ -27,8 +27,8 @@ def squery_db(query, args=()):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
-    if rv is None:
-        return rv
+    if rv is None or len(rv) == 0:
+        return None
     return rv[0]
 
 @app.route("/")
@@ -43,33 +43,42 @@ def get_current_user():
 
     user = squery_db("select * from users where user_id=?", [user_id])
     if not user:
-        return {"error": "server error"}, 409
+        return {"error": "server error"}, 401
 
-    msg = "Welcome, " + user["first_name"] + " " + user["last_name"] + "."
-    return {"msg": msg}, 200
+    payload = {
+        "user_id": user["user_id"],
+        "email": user["email"],
+        "first_name": user["first_name"],
+        "last_name": user["last_name"]
+    }
+
+    return {"user": payload}, 200
 
 @app.route("/register", methods=["POST"])
+@cross_origin()
 def register_user():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if not request.json:
+        return {"error": "bad form"}, 401
+
+    first_name = request.json.get("first_name")
+    last_name = request.json.get("last_name")
+    email = request.json.get("email")
+    password = request.json.get("password")
 
     user = query_db("select * from users where email = ?", [email])
     if user:
-        print("user exists")
-        return {"error": "user already exists"}, 409
+        return {"error": "user already exists"}, 401
 
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-   
-    res = query_db("insert into users (first_name, last_name, email, password) values (?, ?, ?, ?))",
+     
+    res = squery_db("insert into users (first_name, last_name, email, password) values (?, ?, ?, ?)",
              [first_name, last_name, email, hashed_password])
-    if res is not None:
-        return {"error": "error creating account"}, 409
+    if res != None:
+        return {"error": hashed_password}, 401
 
     id = squery_db("select user_id from users where email = ?", [email])
     if id is None:
-        return {"error": "error accessing account id"}, 409
+        return {"error": "error accessing account id"}, 401
 
     session["id"] = id
     session["email"] = email
@@ -78,15 +87,18 @@ def register_user():
 
 @app.route("/login", methods=["POST"])
 def login_user():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if not request.json:
+        return {"error": "bad form"}, 401
+
+    email = request.json.get("email")
+    password = request.json.get("password")
 
     user = squery_db("select * from users where email = ?", [email])
     if user is None:
-        return {"error": "user does not exist"}, 409
+        return {"error": "user does not exist"}, 401
 
     if not bcrypt.check_password_hash(user["password"], password):
-        return {"error": "invalid password"}, 409
+        return {"error": "invalid password"}, 401
 
     session["email"] = user["email"]
     session["id"] = user["id"]
